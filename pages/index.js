7352dda384275b8dd106e8df7f06378b21d38093
Ruggero Tomaselli <ruggerotomaselli@gmail.com>
2022-08-09 09:23:53 +0200

    Categorie dinamiche con costanti

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import { ButtonWithLink } from "components/Button";
 import Title from "components/Title";
+import { categorie } from "lib/const";
 
 export default function Home() {
   return (
@@ -7,9 +8,15 @@ export default function Home() {
       <Title>Categorie</Title>
       <div className="grid justify-items-stretch space-y-4">
         <h3>Seleziona una categoria</h3>
-        <ButtonWithLink href="/men">MEN</ButtonWithLink>
-        <ButtonWithLink href="/mixed">MIXED</ButtonWithLink>
-        <ButtonWithLink href="/women">WOMEN</ButtonWithLink>
+        <ButtonWithLink href={"/" + categorie[0]}>
+          {categorie[0].toUpperCase()}
+        </ButtonWithLink>
+        <ButtonWithLink href={"/" + categorie[1]}>
+          {categorie[1].toUpperCase()}
+        </ButtonWithLink>
+        <ButtonWithLink href={"/" + categorie[2]}>
+          {categorie[2].toUpperCase()}
+        </ButtonWithLink>
       </div>
     </div>
   );
