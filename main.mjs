f03758e58175bbdc31c852439243cc9bbf3221ad
Ruggero Tomaselli <ruggerotomaselli@gmail.com>
2025-03-09 14:58:08 +0100

    òioò

--- main.mjs
+++ main.mjs
@@ -1,146 +1,219 @@
-import { algoritmo } from "./lib/algoritmo.mjs";
+import nextEnv from "@next/env";
 
-const NUMERO_FASI = 3;
-const BASEROW_API_URL = "https://api.baserow.io";
+import { algoritmo } from "./lib/algoritmo.js";
+import { BASEROW_API_URL } from "./lib/baserow.js";
+import { FASI, FASI2 } from "./lib/const.js";
+const { loadEnvConfig } = nextEnv;
 
-const FASI = [
-  "Finalissima",
-  "Finali",
-  "Semifinali",
-  "Quarti",
-  "Ottavi",
-  "Sedicesimi",
-  "Trentaduesimi",
-];
-
-const FASI2 = ["Diretta", "Ripescaggio 1", "Ripescaggio 2"];
+const projectDir = process.cwd();
+loadEnvConfig(projectDir);
 
 const ALPHABET = "abcdefg";
 
-async function groupBy(data, func, squadre) {
-  const baserowToken = process.env["BASEROW_TOKEN"];
-  const data_grouped = Object.groupBy(data, func);
-  for (let fase = 0; fase < 5; fase++) {
-    const fase_str = FASI[fase];
+const BASEROW_TOKEN = process.env["BASEROW_TOKEN"];
+
+const NUMERO_FASI = 4;
+
+async function fetch2(...args) {
+  return fetch(...args);
+}
+
+async function main(categoria) {
+  let res = await fetch(
+    `${BASEROW_API_URL}/api/database/rows/table/333631/?user_field_names=true`,
+    {
+      headers: {
+        Authorization: `Token ${BASEROW_TOKEN}`,
+      },
+    },
+  );
+  const data = (await res.json())["results"];
+  res = await fetch(
+    `${BASEROW_API_URL}/api/database/rows/table/134207/?user_field_names=true&order_by=Nome`,
+    {
+      headers: {
+        Authorization: `Token ${BASEROW_TOKEN}`,
+      },
+    },
+  );
+  const squadre = (await res.json())["results"];
+
+  const data_grouped = Object.groupBy(data, (v) => [
+    v.Fase["value"].substr(1),
+    v["Fase 2"]["value"],
+  ]);
+
+  const nodi = [];
+  for (let fase = 0; fase <= NUMERO_FASI; fase++) {
     for (const fase2 of FASI2) {
-      const nodi = [];
       if (fase2 == FASI2[0]) {
         const _nodi = algoritmo(Math.max(fase, 1));
         for (let i = 0; i < _nodi.length / 2; i++) {
           nodi.push({
             squadra1: _nodi[i * 2],
             squadra2: _nodi[i * 2 + 1],
+            fase,
+            fase2,
+            ordine: i,
           });
         }
-      } else if (fase2 == FASI2[1] && fase > 0) {
+      } else if (fase2 == FASI2[1] && fase > 0 && fase < NUMERO_FASI) {
         const _nodi = algoritmo(Math.max(fase + 1, 2));
         for (let i = 0; i < _nodi.length / 4; i++) {
           nodi.push({
             squadra1: _nodi[i * 4 + 3],
             squadra2: _nodi[i * 4 + 1],
+            fase,
+            fase2,
+            ordine: i,
           });
         }
-      } else if (fase2 == FASI2[2] && fase > 0) {
+      } else if (fase2 == FASI2[2] && fase > 0 && fase < NUMERO_FASI) {
         const _nodi = algoritmo(Math.max(fase + 1, 2));
         for (let i = 0; i < _nodi.length / 4; i++) {
           nodi.push({
             squadra1: _nodi[i * 4 + 2],
             squadra2: _nodi[i * 4 + 3],
+            fase,
+            fase2,
+            ordine: i,
           });
-        }
-      }
-      for (let i = 0; i < nodi.length; i++) {
-        const nodo = nodi[i];
-        const rows = data_grouped[`${fase_str},${fase2}`]?.filter(
-          (v) => v["Ordine"] == i + 1,
-        );
-        if (rows == undefined || rows.length == 0) {
-          // Create the row if no match
-          const res = await fetch(
-            `${BASEROW_API_URL}/api/database/rows/table/333631/?user_field_names=true`,
-            {
-              method: "POST",
-              headers: {
-                Authorization: `Token ${baserowToken}`,
-                "Content-Type": "application/json",
-              },
-              body: JSON.stringify({
-                "Squadra 1": squadre[nodo.squadra1 - 1]["Nome"],
-                "Squadra 2": squadre[nodo.squadra2 - 1]["Nome"],
-                Fase: `${ALPHABET[fase]}${fase_str}`,
-                "Fase 2": fase2,
-                Ordine: i + 1,
-              }),
-            },
-          );
-          console.log(await res.json());
-          continue;
-        }
-        // Check if the first row is aligned
-        if (
-          rows[0]["Squadra 1"][0]["value"] !=
-            squadre[nodo.squadra1 - 1]["Nome"] ||
-          rows[0]["Squadra 2"][0]["value"] != squadre[nodo.squadra2 - 1]["Nome"]
-        ) {
-          const res = await fetch(
-            `${BASEROW_API_URL}/api/database/rows/table/333631/?user_field_names=true`,
-            {
-              method: "PATCH",
-              headers: {
-                Authorization: `Token ${baserowToken}`,
-                "Content-Type": "application/json",
-              },
-              body: JSON.stringify({
-                "Squadra 1": squadre[nodo.squadra1 - 1]["Nome"],
-                "Squadra 2": squadre[nodo.squadra2 - 1]["Nome"],
-              }),
-            },
-          );
-          console.log(await res.json());
-        }
-        // Delete all rows duplicated
-        for (let i = 1; i < rows.length; i++) {
-          await fetch(
-            `${BASEROW_API_URL}/api/database/rows/table/333631/${rows[i]["id"]}/?user_field_names=true`,
-            {
-              method: "DELETE",
-              headers: {
-                Authorization: `Token ${baserowToken}`,
-                "Content-Type": "application/json",
-              },
-            },
-          );
         }
       }
     }
   }
+  // console.log(data_grouped);
+  for (const nodo of nodi) {
+    const fase_str = FASI[nodo.fase];
+    const rows = data_grouped[`${fase_str},${nodo.fase2}`]?.filter(
+      (v) => v["Ordine"] == nodo.ordine + 1,
+    );
+    if (rows == undefined || rows.length == 0) {
+      // Create the row if no match
+      await fetch2(
+        `${BASEROW_API_URL}/api/database/rows/table/333631/?user_field_names=true`,
+        {
+          method: "POST",
+          headers: {
+            Authorization: `Token ${BASEROW_TOKEN}`,
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            "Squadra 1": squadre[nodo.squadra1 - 1]["Nome"],
+            "Squadra 2": squadre[nodo.squadra2 - 1]["Nome"],
+            Fase: `${ALPHABET[nodo.fase]}${fase_str}`,
+            "Fase 2": nodo.fase2,
+            Ordine: nodo.ordine + 1,
+          }),
+        },
+      );
+      continue;
+    }
+    // Check if the first row is aligned
+    if (
+      rows[0]["Squadra 1"][0]["value"] != squadre[nodo.squadra1 - 1]["Nome"] ||
+      rows[0]["Squadra 2"][0]["value"] != squadre[nodo.squadra2 - 1]["Nome"]
+    ) {
+      await fetch2(
+        `${BASEROW_API_URL}/api/database/rows/table/333631/?user_field_names=true`,
+        {
+          method: "PATCH",
+          headers: {
+            Authorization: `Token ${BASEROW_TOKEN}`,
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            "Squadra 1": squadre[nodo.squadra1 - 1]["Nome"],
+            "Squadra 2": squadre[nodo.squadra2 - 1]["Nome"],
+          }),
+        },
+      );
+    }
+  }
 }
 
-async function main() {
-  const initial_data = algoritmo(NUMERO_FASI);
-  const baserowToken = process.env["BASEROW_TOKEN"];
-  const url = `${BASEROW_API_URL}/api/database/rows/table/333631/?user_field_names=true`;
-  let res = await fetch(url, {
-    headers: {
-      Authorization: `Token ${baserowToken}`,
-    },
-  });
-  const data_raw = await res.json();
-  res = await fetch(
-    `${BASEROW_API_URL}/api/database/rows/table/134207/?user_field_names=true&order_by=Nome`,
+async function deleteData(categoria) {
+  const res = await fetch(
+    `${BASEROW_API_URL}/api/database/rows/table/333631/?user_field_names=true`,
     {
       headers: {
-        Authorization: `Token ${baserowToken}`,
+        Authorization: `Token ${BASEROW_TOKEN}`,
       },
     },
   );
-  const squadre_raw = await res.json();
-  // console.log(squadre_raw);
-  const data_grouped = groupBy(
-    data_raw.results,
-    (v, i) => [v.Fase["value"].substr(1), v["Fase 2"]["value"]],
-    squadre_raw.results,
-  );
+  const data = (await res.json())["results"];
+
+  const data_grouped = Object.groupBy(data, (v) => [
+    v.Fase["value"].substr(1),
+    v["Fase 2"]["value"],
+  ]);
+
+  const nodi = [];
+  for (let fase = 0; fase <= NUMERO_FASI; fase++) {
+    for (const fase2 of FASI2) {
+      if (fase2 == FASI2[0]) {
+        const _nodi = algoritmo(Math.max(fase, 1));
+        for (let i = 0; i < _nodi.length / 2; i++) {
+          nodi.push({
+            squadra1: _nodi[i * 2],
+            squadra2: _nodi[i * 2 + 1],
+            fase,
+            fase2,
+            ordine: i,
+          });
+        }
+      } else if (fase2 == FASI2[1] && fase > 0 && fase < NUMERO_FASI) {
+        const _nodi = algoritmo(Math.max(fase + 1, 2));
+        for (let i = 0; i < _nodi.length / 4; i++) {
+          nodi.push({
+            squadra1: _nodi[i * 4 + 3],
+            squadra2: _nodi[i * 4 + 1],
+            fase,
+            fase2,
+            ordine: i,
+          });
+        }
+      } else if (fase2 == FASI2[2] && fase > 0 && fase < NUMERO_FASI) {
+        const _nodi = algoritmo(Math.max(fase + 1, 2));
+        for (let i = 0; i < _nodi.length / 4; i++) {
+          nodi.push({
+            squadra1: _nodi[i * 4 + 2],
+            squadra2: _nodi[i * 4 + 3],
+            fase,
+            fase2,
+            ordine: i,
+          });
+        }
+      }
+    }
+  }
+  // console.log(data_grouped);
+  const data_to_delete = data.slice();
+  for (const nodo of nodi) {
+    const fase_str = FASI[nodo.fase];
+    const rows = data_grouped[`${fase_str},${nodo.fase2}`]?.filter(
+      (v) => v["Ordine"] == nodo.ordine + 1,
+    );
+    if (rows == undefined || rows.length == 0) {
+      continue;
+    }
+    delete data_to_delete[data_to_delete.indexOf(rows[0])];
+  }
+  // console.log(data_to_delete);
+  for (const data_to_d of data_to_delete) {
+    if (!data_to_d) continue;
+    // console.log(data_to_d);
+    await fetch2(
+      `${BASEROW_API_URL}/api/database/rows/table/333631/${data_to_d["id"]}/?user_field_names=true`,
+      {
+        method: "DELETE",
+        headers: {
+          Authorization: `Token ${BASEROW_TOKEN}`,
+          "Content-Type": "application/json",
+        },
+      },
+    );
+  }
 }
 
-await main();
+await deleteData("U13M");
